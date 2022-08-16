2a71acdb5f4a1fbd685006d6147533a354dac6a9
guilhermecat <guizkj77@gmail.com>
2022-08-16 17:28:15 +0000

    Arquivos IaC v.1

---
+++ iac1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Criando diretorios.." 
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /ven
+
+echo "Criando grupos de usuários..."
+
+group GRP_ADM
+group GRP_VEN
+group GRP_SEC
+
+echo "Criando usuarios..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastian -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissões dos diretorios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec 
+chmod 777 /publico
+
+echo "Fim...."
